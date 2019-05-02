3be7fe073187665acceeded02c2c001e6f605642
William <informatica01@COOPER.local>
2019-05-02 15:55:44 -0300

    Controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura=0
 var largura=0
+var vidas=1
 
 function ajustaTamanhoPalcoJogo(){
 	altura=window.innerHeight
@@ -15,6 +16,13 @@ function posicaoRandomica(){
 	//remover mosquito anterior caso exista
 	if(document.getElementById('mosquito')){
 		document.getElementById('mosquito').remove()
+
+		if(vidas>3){
+			alert('imterromper jogo')
+		}else{
+			document.getElementById('v'+vidas).src="imagens/coracao_vazio.png"
+			vidas++
+		}
 	}
 
 	var posicaoX=Math.floor(Math.random()*largura)-90
@@ -33,6 +41,10 @@ function posicaoRandomica(){
 	mosquito.style.top=posicaoY+'px'
 	mosquito.style.position='absolute'
 	mosquito.id='mosquito'
+	mosquito.onclick=function(){
+		this.remove()
+	}
+
 	//INCLUINDO ELEMENTO NO BODY DA PAGINA
 	document.body.appendChild(mosquito)
 }
